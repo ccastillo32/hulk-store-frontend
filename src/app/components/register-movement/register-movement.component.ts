7bafc880eecd7c39b86435a331eb70a4b2596d8f
ccastillo32 <cristiancastillo32@gmail.com>
2021-01-06 09:52:33 -0500

    Some adjustments

--- src/app/components/register-movement/register-movement.component.ts
+++ src/app/components/register-movement/register-movement.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { InventoryItem } from 'src/app/model/inventory-item.model';
+import { RoutingService } from 'src/app/routing/routing.service';
 
 @Component({
     selector: 'register-movement',
@@ -7,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
 
 export class RegisterMovementComponent implements OnInit {
 
-    constructor() {}
+    loading: boolean = false;
+    formSubmitted: boolean = false;
+
+    registerMovementForm = new FormGroup({
+        code: new FormControl('', [Validators.required]),
+        name: new FormControl('', [Validators.required]),
+        quantity: new FormControl(0, [Validators.required]),
+        unitPrice: new FormControl(0, [Validators.required]),
+        subTotal: new FormControl(0, [Validators.required]), 
+        observation: new FormControl('', []),
+    });
+
+    constructor(
+        private routingService: RoutingService
+    ) {}
 
     ngOnInit() {
-        
+         
+    }
+
+    cancel(): void {
+        this.routingService.goToProductList();
+    }
+
+    isEmpty(fieldName: string): boolean {
+        return this.registerMovementForm.get(fieldName).hasError('required') 
+                && (this.registerMovementForm.get(fieldName).touched || this.formSubmitted)
+    }
+
+    isNotGreaterThanZero(fieldName: string): boolean {
+        return this.registerMovementForm.get(fieldName).hasError('min') 
+                && (this.registerMovementForm.get(fieldName).touched || this.formSubmitted)
     }
 
 }
