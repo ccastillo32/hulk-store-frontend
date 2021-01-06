1d340058effbde633a357d19fdf3fdd3027dae7a
ccastillo32 <cristiancastillo32@gmail.com>
2021-01-06 09:03:04 -0500

    Show the current inventory info on the home page

--- src/app/services/implementation/inventory.service.impl.ts
+++ src/app/services/implementation/inventory.service.impl.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs";
+import { forkJoin, Observable } from "rxjs";
 import { map } from "rxjs/operators";
+
 import { InventoryItem } from "../../model/inventory-item.model";
 import { Product } from "../../model/product.model";
 import { InventoryService } from "../inventory.service";
 import { ProductService } from '../product.service';
 import { HttpService } from './http.service';
+import { InventoryInfo } from '../../model/inventory-info.model';
 
 @Injectable({
     providedIn: 'root'
@@ -14,26 +16,43 @@ import { HttpService } from './http.service';
 export class InventoryRestApiService extends InventoryService {
 
     constructor(
-        private productService: ProductService    
+        private productService: ProductService,
+        private httpService: HttpService    
     ) {
         super();
     }
 
     public findAllInventoryItems(): Observable<InventoryItem[]> {
 
-        return this.productService.findAllProducts().pipe(
-            map( (allProducts: Product[]) => {
+        const allProducts$: Observable<Product[]> = this.productService.findAllProducts();
 
-                return allProducts.map((product: Product) => {
+        const url: string = 'http://localhost:9099/api/inventory-info';
+        const allInventoryInfo$: Observable<InventoryInfo[]> = this.httpService.get(url).pipe(
+            map( (response: any) => {
+                return response.items as InventoryInfo[]
+            })
+        );
+
+        const fork$: Observable<InventoryItem[]> = forkJoin([ allProducts$, allInventoryInfo$ ]).pipe(
+            map(([ allProducts, allInventoryInfo ]) => {
+
+                return allProducts.map( (product: Product) => {
+
+                    const info: InventoryInfo = allInventoryInfo.find(i => i.productId === product.id);
+
                     let item: InventoryItem = new InventoryItem();
                     item.product = product;
-                    item.incomings = 0;
-                    item.outgoings = 0;
+                    item.incomings = info.incomings;
+                    item.outgoings = info.outgoings;
+
                     return item;
+
                 })
 
             })
         );
+
+        return fork$;
 
     }
 
